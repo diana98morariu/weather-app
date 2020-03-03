656e399a1b0a912e28f6fb2b23a81dab54e6bd3f
Diana Morariu <you@example.com>
2020-03-03 18:25:25 +0100

    other icons

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,24 +1,27 @@
 import React from "react";
+import { FaTemperatureHigh } from "react-icons/fa";
+import { TiLocation, TiWeatherPartlySunny } from "react-icons/ti";
+import { WiHumidity } from "react-icons/wi";
 
 const Weather = props => (
     <div className="weather__info">
         {
-            props.city && props.country && <p className="weather__key">Location: 
+            props.city && props.country && <p className="weather__key"> <TiLocation /> Location: 
                 <span className="weather__value"> {props.city}, {props.country}</span>
             </p>
         }
         {
-            props.temperature && <p className="weather__key">Temperature: 
+            props.temperature && <p className="weather__key"> <FaTemperatureHigh /> Temperature: 
                 <span className="weather__value"> {props.temperature}</span>
             </p>
         }
         {
-            props.humidity && <p className="weather__key">Humidity: 
+            props.humidity && <p className="weather__key"> <WiHumidity /> Humidity: 
                 <span className="weather__value"> {props.humidity}</span>
             </p>
         }
         {
-            props.description && <p className="weather__key">Conditions: 
+            props.description && <p className="weather__key"> <TiWeatherPartlySunny /> Conditions: 
                 <span className="weather__value"> {props.description}</span>
             </p>
         }
